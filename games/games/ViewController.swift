851d678b3edbcabd6e284f3f7e58ae243c1dfabc
Kaan Kaya <kaan.kaya@ordulu.com.tr>
2021-07-04 20:50:46 +0300

    Initial commit

---
+++ games/games/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  games
+//
+//  Created by Kaan Kaya on 4.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
